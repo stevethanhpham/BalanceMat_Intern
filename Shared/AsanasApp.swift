4b10a9946134bbae98fd6105d1807215a15d7b88
Duong Pham <stev.thanhpham@gmail.com>
2021-11-25 15:22:28 +1100

    Initial Commit

---
+++ Shared/AsanasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsanasApp.swift
+//  Shared
+//
+//  Created by Steve Pham on 25/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AsanasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
